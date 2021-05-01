e7a682452711886777bff34d9c9fe20b336d58b5
Roy-Roo <79677973+Roy-Roo@users.noreply.github.com>
2021-05-01 16:33:39 +0800

    Update vector.js

--- week10/HW3/vector.js
+++ week10/HW3/vector.js
@@ -56,7 +56,7 @@ console.log('x.add(y)=',x.add(y))
 console.log('x.sub(y)=',x.sub(y))
 console.log('x.dot(y)=',x.dot(y))
 console.log('x.neg(y)=',x.neg(y))
-`````
+```
 ##  執行結果
 ```
 x.add(y)= vector { a: [ 3, 3, 3 ] }
